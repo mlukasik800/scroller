f8a73d20d2aff9fec0b4c1a919f78e32ef1282f8
mlukasik800 <mlukasik800@gmail.com>
2021-10-22 16:10:53 +0200

    Add navigation to the scroller feature

--- Scroller.js
+++ Scroller.js
@@ -4,9 +4,12 @@ class Scroller {
         this.sections = document.querySelectorAll('section');
         const sectionsArr = [...this.sections];
         const currentSectionIndex = sectionsArr.findIndex(this.isScrolledIntoView);
+        this.currentSectionIndex = Math.max(currentSectionIndex, 0);
+
         this.isThrottled = false;
 
-        this.currentSectionIndex = Math.max(currentSectionIndex, 0);
+        this.drawNavigation();
+
     }
 
     isScrolledIntoView(element) {
@@ -47,9 +50,51 @@ class Scroller {
         this.scrollToCurrentSection()
     }
     scrollToCurrentSection = () => {
+        this.selectActiveNavItem()
         this.sections[this.currentSectionIndex].scrollIntoView({
             behavior: 'smooth',
             block: "start",
         })
+
+
+    }
+    drawNavigation = () => {
+        this.navigationContainer = document.createElement('aside');
+        this.navigationContainer.setAttribute('class', 'scroller__navigation');
+        const list = document.createElement('ul');
+
+        this.sections.forEach((section, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.addEventListener('click', () => {
+                this.currentSectionIndex = index;
+
+                this.scrollToCurrentSection()
+            })
+
+            list.appendChild(listItem);
+        })
+
+        this.navigationContainer.appendChild(list)
+
+        document.body.appendChild(this.navigationContainer)
+        this.selectActiveNavItem()
+
+    }
+
+    selectActiveNavItem = () => {
+        if (this.navigationContainer) {
+            const navigationItems = this.navigationContainer.querySelectorAll('li');
+
+            navigationItems.forEach((item, index) => {
+                if (index === this.currentSectionIndex) {
+                    item.classList.add('active')
+                } else {
+                    item.classList.remove('active');
+                }
+            })
+        }
+
+
     }
 }
